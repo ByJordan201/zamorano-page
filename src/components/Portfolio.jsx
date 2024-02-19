bc125e050e3e04c2c11d15cd7d04abaa93b0294f
JessicaVillanueva <51139862+JessicaVillanueva@users.noreply.github.com>
2024-02-18 20:29:55 -0700

    acomodo de proyectos de MÁS ANTIGUO AL MÁS RECIENTE

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,54 +7,53 @@ const Portfolio = () => {
     const ProjectData01 = [
         {
             id: '1',
-            img: 'images/portfolio/packery/2.jpg',
-            location: 'Los Cabos, B.C.S., 2023',
-            details: 'Casa Grieve',
-            tag: 'interiors'
+            img: 'images/portfolio/packery/8.jpg',
+            location: 'Los Cabos, B.C.S., 2019',
+            details: 'Casa Caplan',
+            tag: 'exterior'
         },
         {
             id: '2',
-            img: 'images/portfolio/packery/3.jpg',
-            location: 'La Paz, B.C.S., 2022',
-            details: 'Hotel Chablé Mar de Cortés',
-            tag: 'residences'
-        },
-        {
-            id: '4',
-            img: 'images/portfolio/packery/5.jpg',
-            location: 'La Paz, B.C.S. 2022',
-            details: 'CHIREY',
-            tag: 'landscape'
-        },
-        {
-            id: '5',
-            img: 'images/portfolio/packery/6.jpg',
-            location: 'Los Cabos, B.C.S. 2022',
-            details: 'Fundadores 26',
-            tag: 'interiors'
-        },
-        {
-            id: '6',
-            img: 'images/portfolio/packery/7.jpg',
-            location: 'Los Cabos, B.C.S. 2022',
-            details: 'Lote 24',
-            tag: 'interiors'
-        },
-        {
-            id: '7',
             img: 'images/portfolio/packery/Agora.jpg',
             location: 'Hermosillo, Sonora 2021',
             details: 'Ágora',
             tag: 'exterior'
         },
         {
-            id: '8',
-            img: 'images/portfolio/packery/8.jpg',
-            location: 'Los Cabos, B.C.S., 2019',
-            details: 'Casa Caplan',
-            tag: 'exterior'
+            id: '3',
+            img: 'images/portfolio/packery/7.jpg',
+            location: 'Los Cabos, B.C.S. 2022',
+            details: 'Lote 24',
+            tag: 'interiors'
         },
-
+        {
+            id: '4',
+            img: 'images/portfolio/packery/6.jpg',
+            location: 'Los Cabos, B.C.S. 2022',
+            details: 'Fundadores 26',
+            tag: 'interiors'
+        },
+        {
+            id: '5',
+            img: 'images/portfolio/packery/5.jpg',
+            location: 'La Paz, B.C.S. 2022',
+            details: 'CHIREY',
+            tag: 'landscape'
+        },
+        {
+            id: '6',
+            img: 'images/portfolio/packery/3.jpg',
+            location: 'La Paz, B.C.S., 2022',
+            details: 'Hotel Chablé Mar de Cortés',
+            tag: 'residences'
+        },
+        {
+            id: '7',
+            img: 'images/portfolio/packery/2.jpg',
+            location: 'Los Cabos, B.C.S., 2023',
+            details: 'Casa Grieve',
+            tag: 'interiors'
+        },
     ];
 
     const [filterKey, setFilterKey] = useState('*')
