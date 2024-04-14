67df7573ce57bc223c395038c9a80df52c598d0b
JessicaVillanueva <51139862+JessicaVillanueva@users.noreply.github.com>
2024-04-13 17:40:17 -0700

    envio de info por wa

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 
 const Form = () => {
+
+     // Función para manejar el envío del formulario
+     const handleSubmit = (event) => {
+        event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+        // Capturar los valores de los campos del formulario
+        const nombre = document.getElementById("name").value;
+        const mensaje = document.getElementById("message").value;
+
+        // Construir el mensaje de WhatsApp con el nombre y la descripción
+        const mensajeWhatsApp = `${nombre}
+        ${mensaje}`; // %0A se utiliza para agregar un salto de línea en el mensaje
+
+        // Construir el enlace de WhatsApp con el mensaje personalizado
+        const enlaceWhatsApp = `https://wa.me/526121682143?text=${encodeURIComponent(mensajeWhatsApp)}`;
+
+        // Abrir el enlace en una nueva pestaña
+        window.open(enlaceWhatsApp, "_blank");
+    };
     return (
         <div className="home_contact">
-            <form action="contact.php" method="POST">
+            <form onSubmit={handleSubmit} action="contact.php" method="POST">
                 <input className="form-control form-control-lg" name="name" id="name" type="text" placeholder="Nombre completo" required aria-label=".form-control-lg example" />
-                <input className="form-control form-control-lg" name="phone" id="phone" type="tel" placeholder="Número de teléfono" aria-label=".form-control-lg example" />
-                <input className="form-control form-control-lg" name="email" id="email" type="email" placeholder="Correo electrónico" required aria-label=".form-control-lg example" />
+                {/* <input className="form-control form-control-lg" name="phone" id="phone" type="tel" placeholder="Número de teléfono" aria-label=".form-control-lg example" />
+                <input className="form-control form-control-lg" name="email" id="email" type="email" placeholder="Correo electrónico" required aria-label=".form-control-lg example" /> */}
                 <textarea className="form-control pt-4" name="message" id="message" placeholder="Describe brevemente tu proyecto" rows="3"></textarea>
                 <div className="btn_group">
-                    <button type="submit" className="btn olive">Enviar</button>
+                    {/* <a href="https://wa.me/526121682143" target='_blank' className="btn olive">Enviar</a> */}
+                    <button  type="submit" className="btn olive" value="Enviar mensaje">Enviar</button>
                 </div>
             </form>
         </div>
